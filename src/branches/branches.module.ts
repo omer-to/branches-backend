b7d270d6d06e4ab1257b1f047b447e7cde29687b
Omer Toraman <omer.toraman@akinon.com>
2021-08-24 21:02:05 +0300

    Generate branches controller: nest g co branches

--- src/branches/branches.module.ts
+++ src/branches/branches.module.ts
@@ -1,4 +1,7 @@
 import { Module } from '@nestjs/common';
+import { BranchesController } from './branches.controller';
 
-@Module({})
+@Module({
+  controllers: [BranchesController]
+})
 export class BranchesModule {}
