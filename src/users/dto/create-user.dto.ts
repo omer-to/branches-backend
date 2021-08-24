0d0ce31936c1358a4847a70d630caee5f8734121
Omer Toraman <omer.toraman@akinon.com>
2021-08-24 22:40:09 +0300

    Add role property to CreateUserDto

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,5 +1,10 @@
 import { IsEmail, IsString, MinLength } from 'class-validator'
 
+// TODO: Move to a separate file
+enum UserRole {
+      employee = 'employee',
+      employer = 'employer',
+}
 export class CreateUserDto {
       @IsEmail()
       email: string
@@ -7,4 +12,7 @@ export class CreateUserDto {
       @IsString()
       @MinLength(8)
       password: string
+
+      // TODO: Validate for enums
+      role: UserRole
 }
