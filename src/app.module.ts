b8ceb90c3b90459312f66387c253ec21dae82374
Omer Toraman <omer.toraman@akinon.com>
2021-08-24 22:21:24 +0300

    Use .env and connect to MongoDB

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,25 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config'
+import { MongooseModule } from '@nestjs/mongoose';
+
 import { BranchesModule } from './branches/branches.module';
 import { UsersModule } from './users/users.module';
 
+
 @Module({
-      imports: [BranchesModule, UsersModule],
+      imports:
+            [
+                  ConfigModule.forRoot(),
+                  MongooseModule.forRoot(
+                        process.env.MONGO_CONNECTION_URI,
+                        {
+                              useNewUrlParser: true,
+                              useUnifiedTopology: true
+                        }
+                  ),
+                  BranchesModule,
+                  UsersModule
+            ],
       controllers: [],
       providers: [],
 })
