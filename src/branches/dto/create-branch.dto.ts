ab06e8269d417b6ee83e9837c77d086bac41f46e
Omer Toraman <omer.toraman@akinon.com>
2021-08-24 21:18:29 +0300

    Update CreateBranchDto with classvalidator validations

--- src/branches/dto/create-branch.dto.ts
+++ src/branches/dto/create-branch.dto.ts
@@ -1,1 +1,19 @@
-export class CreateBranchDto {}
+import { IsLatitude, IsLongitude, IsString, IsPhoneNumber, MinLength } from 'class-validator'
+
+export class CreateBranchDto {
+      @IsString()
+      fullAddress: string
+
+      @IsLatitude({ message: 'Latitude must be between -90 and 90.' })
+      latitude: number
+
+      @IsLongitude({ message: 'Longitude must be between -180 and 180.' })
+      longitude: number
+
+      @IsString()
+      @MinLength(2)
+      name: string
+
+      @IsPhoneNumber('TR')
+      phone: string
+}
