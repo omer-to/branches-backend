a9a66c962c6c7323abf8c1ed3bc4a8b0a4fa5289
Omer Toraman <omer.toraman@akinon.com>
2021-08-24 21:02:34 +0300

    Generate branches service: nest g s branches

--- src/branches/branches.module.ts
+++ src/branches/branches.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { BranchesController } from './branches.controller';
+import { BranchesService } from './branches.service';
 
 @Module({
-  controllers: [BranchesController]
+  controllers: [BranchesController],
+  providers: [BranchesService]
 })
 export class BranchesModule {}
