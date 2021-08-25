b8b37f4fe02e4120853bbc8572c43458a9df335d
Omer Toraman <omer.toraman@akinon.com>
2021-08-25 17:13:54 +0300

    Refactor role-guard

--- src/guards/role.guard.ts
+++ src/guards/role.guard.ts
@@ -1,19 +1,18 @@
 import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
-import { Observable } from 'rxjs';
 import { Reflector } from '@nestjs/core';
-
 
 @Injectable()
 export class RoleGuard implements CanActivate {
       constructor(private reflector: Reflector) { }
 
-      canActivate(context: ExecutionContext): boolean | Promise<boolean> | Observable<boolean> {
+      canActivate(context: ExecutionContext): boolean {
             const roles = this.reflector.get<string[]>('roles', context.getHandler());
             if (!roles) {
-              return true;
+                  return true;
             }
-            const request = context.switchToHttp().getRequest();
-            const user = request.user;
-            return true;
+            const request = context.switchToHttp().getRequest(),
+                  user = request.user;
+
+            return user.role === 'employer';
       }
 }
