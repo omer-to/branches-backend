28d64e7e50d74cb03690b90517f57d55cc4c9ec6
Omer Toraman <omer.toraman@akinon.com>
2021-08-25 04:36:31 +0300

    Scaffold other HTTP methods for branches

--- src/branches/branches.controller.ts
+++ src/branches/branches.controller.ts
@@ -1,14 +1,37 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, UseGuards } from '@nestjs/common';
 
+import { Roles } from '../decorators/roles.decorator';
+import { RoleGuard } from '../guards/role.guard';
 import { BranchesService } from './branches.service';
 import { CreateBranchDto } from './dto/create-branch.dto';
+import { UpdateBranchDto } from './dto/update-branch.dto';
 
 @Controller('branches')
 export class BranchesController {
       constructor(private readonly branchesService: BranchesService) { }
 
       @Post()
+      @Roles('employer')
+      @UseGuards(RoleGuard)
       create(@Body() requestBody: CreateBranchDto) {
             return this.branchesService.create(requestBody);
+      }
+
+      @Get(':branchID')
+      findByID(@Param('branchID') branchID: string) {
+            return this.branchesService.findByID(branchID);
+      }
+
+      @Patch(':branchID')
+      updateByID(
+            @Param('branchID') branchID: string,
+            @Body() requestBody: UpdateBranchDto
+      ) {
+            return this.branchesService.updateByID(branchID, requestBody)
+      }
+
+      @Delete(':branchID')
+      deleteByID(@Param('branchID') branchID) {
+            return this.branchesService.deleteByID(branchID)
       }
 }
--- src/branches/branches.service.ts
+++ src/branches/branches.service.ts
@@ -1,10 +1,28 @@
+import { Model } from 'mongoose'
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose'
 
+import { Branch, BranchDocument } from './schemas/branch.schema'
 import { CreateBranchDto } from './dto/create-branch.dto';
+import { UpdateBranchDto } from './dto/update-branch.dto';
 
 @Injectable()
 export class BranchesService {
+      constructor(@InjectModel(Branch.name) private branchModel: Model<BranchDocument>) { }
+
       create(requestBody: CreateBranchDto) {
             return requestBody
+      }
+
+      findByID(branchID: string) {
+            return branchID
+      }
+
+      updateByID(branchID: string, requestBody: UpdateBranchDto) {
+            return { branchID, requestBody }
+      }
+
+      deleteByID(branchID: string) {
+            return branchID
       }
 }
