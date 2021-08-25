24dbb6563c859f0bb9f1439c0067a8807491588c
Omer Toraman <omer.toraman@akinon.com>
2021-08-25 17:15:27 +0300

    Use AuthController for signIn and signUp

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,30 @@
+import { Body, Controller, Post, UseGuards } from '@nestjs/common'
+
+import { Public } from '../decorators/public-route.decorator';
+import { CreateUserDto } from '../users/dto/create-user.dto';
+import { UsersService } from '../users/users.service';
+import { JwtAuthGuard } from '../guards/jwt-auth.guard'
+import { LocalAuthGuard } from '../guards/local-auth.guard'
+import { AuthService } from './auth.service'
+
+
+@Controller()
+@Public()
+export class AuthController {
+      constructor(private readonly authService: AuthService, private readonly usersService: UsersService) { }
+
+      @Post('/signup')
+      async signUp(@Body() requestBody: CreateUserDto): Promise<{ userID: string }> {
+            const user = await this.authService.signUp(requestBody)
+            return { userID: user.id }
+      }
+
+      @UseGuards(LocalAuthGuard)
+      @Public()
+      @Post('/signin')
+      async signIn(@Body() requestBody: Omit<CreateUserDto, 'role'>): Promise<{ userID: string, access_token: string }> {
+            console.log('AuthController signIn')
+            const { access_token, userID } = await this.authService.signIn(requestBody);
+            return { access_token, userID }
+      }
+}
