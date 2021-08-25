5317c049733fed16aac33ab43d3023b9b2c3bb4a
Omer Toraman <omer.toraman@akinon.com>
2021-08-25 17:00:40 +0300

    Add more methods for userService

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -7,10 +7,18 @@ import { User, UserDocument } from './schemas/user.schema'
 
 @Injectable()
 export class UsersService {
-      constructor(@InjectModel(User.name) private branchModel: Model<UserDocument>) { }
+      constructor(@InjectModel(User.name) private userModel: Model<UserDocument>) { }
 
       async create(requestBody: CreateUserDto) {
-            const newUser = new this.branchModel(requestBody)
+            const newUser = new this.userModel(requestBody)
             return newUser.save()
+      }
+
+      async findByID(userID: string) {
+            return this.userModel.findById(userID).exec()
+      }
+
+      async findByEmail(email: string) {
+            return this.userModel.findOne({ email }).exec()
       }
 }
