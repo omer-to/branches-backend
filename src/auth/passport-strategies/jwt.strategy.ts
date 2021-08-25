1ab4e34d98255b1222015212011adfcc35ec36a5
Omer Toraman <omer.toraman@akinon.com>
2021-08-25 17:10:47 +0300

    Refactor

--- src/auth/passport-strategies/jwt.strategy.ts
+++ src/auth/passport-strategies/jwt.strategy.ts
@@ -2,6 +2,8 @@ import { ExtractJwt, Strategy } from 'passport-jwt';
 import { PassportStrategy } from '@nestjs/passport';
 import { Injectable } from '@nestjs/common';
 
+import type { JwtPayload } from 'jsonwebtoken'
+
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
       constructor() {
@@ -12,7 +14,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
             });
       }
 
-      async validate(payload: any) {
-            return { userId: payload.sub, username: payload.username };
+      async validate(payload: JwtPayload) {
+            return { userId: payload.sub, email: payload.email, role: payload.role };
       }
 }
