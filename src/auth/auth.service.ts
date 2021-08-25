c57dcd40caaf1e243f2d0c96933b28461734c7b6
Omer Toraman <omer.toraman@akinon.com>
2021-08-25 17:09:39 +0300

    Refactor & remove logs

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,26 +1,36 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
+import * as bcrypt from 'bcrypt';
 
+import { CreateUserDto } from 'src/users/dto/create-user.dto';
 import { UsersService } from '../users/users.service'
 
 @Injectable()
 export class AuthService {
       constructor(private usersService: UsersService, private jwtService: JwtService) { }
 
-      async validateUser(username: string, pass: string): Promise<any> {
-            // const user = await this.usersService.findOne(username);
-            const user = { password: '' }
-            if (user && user.password === pass) {
-                  const { password, ...result } = user;
-                  return result;
-            }
-            return null;
+      async signUp(createUserDto: CreateUserDto) {
+            const saltOrRounds = 10;
+            const hashedPassword = await bcrypt.hash(createUserDto.password, saltOrRounds)
+            return this.usersService.create({ ...createUserDto, password: hashedPassword })
       }
 
-      async login(user: any) {
-            const payload = { email: user.email, sub: user.userId };
-            return {
-                  access_token: this.jwtService.sign(payload),
-            };
+      async signIn(requestBody: Omit<CreateUserDto, 'role'>) {
+            const user = await this.usersService.findByEmail(requestBody.email)
+            if (!user) {
+                  throw new UnauthorizedException('Invalid Credentials')
+            }
+            try {
+                  const match = await bcrypt.compare(requestBody.password, user.password)
+                  if (match) {
+                        const payload = { email: user.email, sub: user.id, role: user.role };
+                        return {
+                              access_token: this.jwtService.sign(payload),
+                              userID: user.id
+                        };
+                  }
+            } catch (error) {
+                  throw new UnauthorizedException('Invalid Credentials')
+            }
       }
 }
