d3da4af634526b56207112a574cc5da86446f06e
Omer Toraman <omer.toraman@akinon.com>
2021-08-25 17:14:56 +0300

    Remove unused bindings

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,18 +1,13 @@
-import { Body, Controller, Post, UseGuards } from '@nestjs/common';
-import { AuthGuard } from '@nestjs/passport';
-
-import { Public } from '../decorators/public-route.decorator';
-import { CreateUserDto } from './dto/create-user.dto';
+import { Controller, Get, Param, } from '@nestjs/common';
 import { UsersService } from './users.service';
 
 @Controller('users')
 export class UsersController {
       constructor(private readonly usersService: UsersService) { }
 
-      @Post()
-      @Public()
-      async create(@Body() createUserDto: CreateUserDto) {
-            return await this.usersService.create(createUserDto);
+      @Get(':userID')
+      findByID(@Param('userID') userID: string) {
 
+            return this.usersService.findByID(userID)
       }
 }
