47fe0a7f68a7bdf5c56b2b6bcd4a1ff507d9fc87
Omer Toraman <omer.toraman@akinon.com>
2021-08-25 04:09:53 +0300

    Add swagger configuration

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,28 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 import { AppModule } from './app.module';
 import { AppExceptionFilter } from './filters/app-exception.filter';
 
 async function bootstrap() {
       const app = await NestFactory.create(AppModule);
+
       app.useGlobalPipes(
             new ValidationPipe()
       )
       app.useGlobalFilters(
             new AppExceptionFilter()
       )
+
+      const config = new DocumentBuilder()
+            .setTitle('Company Branches')
+            .setDescription('The API for the assignment')
+            .setVersion('1.0')
+            .build();
+      const document = SwaggerModule.createDocument(app, config);
+      SwaggerModule.setup('api', app, document);
+
       await app.listen(3000);
 }
 bootstrap();
