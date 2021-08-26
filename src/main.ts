e00c9c980a8385c27c07fab8b4af2accd2825079
Omer Toraman <omer.toraman@akinon.com>
2021-08-27 00:12:46 +0300

    Enable cors (unconditionally)

--- src/main.ts
+++ src/main.ts
@@ -11,6 +11,8 @@ import { AppExceptionFilter } from './filters/app-exception.filter';
 async function bootstrap() {
       const app = await NestFactory.create(AppModule);
 
+      app.enableCors()
+
       app.useGlobalPipes(
             new ValidationPipe()
       )
@@ -26,6 +28,6 @@ async function bootstrap() {
       const document = SwaggerModule.createDocument(app, config);
       SwaggerModule.setup('api', app, document);
 
-      await app.listen(3000);
+      await app.listen(8000);
 }
 bootstrap();
